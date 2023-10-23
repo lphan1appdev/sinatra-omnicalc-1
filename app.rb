a77cf03c8c7ae2b1da1ae5bd0de1afa39c3ad419
lphan1appdev <146215029+lphan1appdev@users.noreply.github.com>
2023-10-23 01:44:49 +0000

    query strings

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
