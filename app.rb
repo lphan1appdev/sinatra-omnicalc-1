ee1e1a0b94e6b78410e3494a2639f819cf8332d0
lphan1appdev <146215029+lphan1appdev@users.noreply.github.com>
2023-10-23 02:20:05 +0000

    omni

--- app.rb
+++ app.rb
@@ -9,8 +9,17 @@ get("/goodbye") do
   erb(:bye)
 end
 
+get("/square/new") do
+  erb(:new_square_calc)
+end
 
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
 
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
 
 get("/") do
   "
