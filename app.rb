6f2e2fdef29fd5c57b7f5e20107651fd2382715a
lphan1appdev <146215029+lphan1appdev@users.noreply.github.com>
2023-10-23 02:53:08 +0000

    stuff

--- app.rb
+++ app.rb
@@ -21,6 +21,20 @@ get("/square/results") do
   erb(:square_results)
 end
 
+get("/square_root/new") do
+  erb(:square_root_new)
+
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_root_new_results)
+
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
