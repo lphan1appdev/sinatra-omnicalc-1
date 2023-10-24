695f5a9cbfb521ccd0df98b16ecd459e9b29a7bd
lphan1appdev <146215029+lphan1appdev@users.noreply.github.com>
2023-10-24 02:05:36 +0000

    changes

--- app.rb
+++ app.rb
@@ -35,6 +35,41 @@ get("/square_root/results") do
 
 end
 
+get("/payment/new") do
+  erb(:payment_new)
+
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f
+  @years = params.fetch("years").to_i
+  @principal = params.fetch("principal").to_f
+
+  # Convert APR to a monthly rate
+  monthly_rate = (@apr / 100.0) / 12.0
+
+  # Convert years to months
+  number_of_payments = @years * 12
+
+  # Calculate the monthly payment
+  @the_result = @principal * (monthly_rate * (1 + monthly_rate) ** number_of_payments) / ((1 + monthly_rate) ** number_of_payments - 1)
+
+  erb(:payment_new_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+
+end
+
+get("/random/results") do
+  @min_num = params.fetch("min_input").to_i
+  @max_num = params.fetch("max_input").to_i
+  @random_num = rand(@min_num..@max_num)
+
+  erb(:random_new_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
